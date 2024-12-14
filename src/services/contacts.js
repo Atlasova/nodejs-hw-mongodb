8e3788ea087388aba10bb3b6fd16b72fa1d558c1
Juliia Prokopenko <atlasovaa13@gmail.com>
2024-12-14 16:26:34 +0100

    homework-3

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,32 @@ export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, payload) => {
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+    },
+  );
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult.lastErrorObject.upserted),
+  };
+};
+
+export const deleteContactById = async (contactId) => {
+  const contact = await ContactsCollection.findByIdAndDelete({
+    _id: contactId,
+  });
+  return contact;
+};
